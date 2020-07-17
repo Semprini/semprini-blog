224c0a39d1833e2a9b24abad2971998af22ad569
Semprini <atkinpa@gmail.com>
2020-07-17 04:45:34 +0000

    Initial blog

---
+++ app/semprini/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'nz(59v4wz(ytpxqg(6je!98!*#4*etke+(^xxv2!zno832$$g+'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
