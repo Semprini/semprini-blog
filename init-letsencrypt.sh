1d705a616df9fc3dd124a5f33af9baed7138dad1
Semprini <atkinpa@gmail.com>
2021-03-29 09:12:28 +0000

    added dataiku and pgadmin

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(semprini.ml www.semprini.ml)
+domains=(semprini.ml www.semprini.ml analytics.sphinx.co.nz rancher.sphinx.co.nz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="paul@sphinx.co.nz" # Adding a valid address is strongly recommended
