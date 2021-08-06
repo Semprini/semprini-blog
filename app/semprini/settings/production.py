de0fded6fc279ecbd9aa6997d91cd2c1aadce2d6
Semprini <atkinpa@gmail.com>
2021-08-07 01:53:59 +1200

    ssl offload moved to host

--- app/semprini/settings/production.py
+++ app/semprini/settings/production.py
@@ -2,8 +2,9 @@ from .base import *
 
 DEBUG = False
 
-SECRET_KEY = "n9*fw(b3yzc3lliu-=-6r9)qvjjj(e_!f@kb-r+mk-+ql)iy!%"
+SECRET_KEY = os.environ.get("SECRET_KEY", "n9*fw(b3yzc3lliu-=-6r9)qvjjj(e_!f@kb-r+mk-+ql)iy!%")
 ALLOWED_HOSTS = ['*']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 try:
     from .local import *
