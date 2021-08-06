92c240d5a26a79f6bb42e9c4debf5ce400c94ab4
Semprini <atkinpa@gmail.com>
2021-08-07 02:07:39 +1200

    ssl offload moved to host

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec python manage.py migrate
-exec gunicorn semprini.wsgi:application --bind 0.0.0.0:8000 --workers 3
+python manage.py migrate
+gunicorn semprini.wsgi:application --bind 0.0.0.0:8000 --workers 3
