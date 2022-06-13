5816159b1b699cb118d40ecffbb7d8e62721a5b0
Semprini <atkinpa@gmail.com>
2022-06-13 14:47:39 +1200

    Added CSRF trusted setting (Django 4.0)

--- app/semprini/settings/production.py
+++ app/semprini/settings/production.py
@@ -5,6 +5,7 @@ DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY", None)
 ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+CSRF_TRUSTED_ORIGINS = ['https://semprini.me']
 
 try:
     from .local import *
